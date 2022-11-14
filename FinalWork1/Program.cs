263944c834bd64c1c58bdaa1c67f2b0d0adde5d2
Walkmaster <repouched@gmail.com>
2022-11-14 18:06:39 +0500

    createMethod

--- FinalWork1/Program.cs
+++ FinalWork1/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] words = new string[5] {"Hello", "my", "name", "is", "Sanya"};
 string[] needlyWords = new string[words.Length];
+
+void GetNeedlyWords(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
