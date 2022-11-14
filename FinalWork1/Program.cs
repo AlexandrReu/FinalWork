baf8936e1b7ea7e4393a488256f992c1c1be2181
Walkmaster <repouched@gmail.com>
2022-11-14 18:17:15 +0500

    complite

--- FinalWork1/Program.cs
+++ FinalWork1/Program.cs
@@ -1,15 +1,28 @@
-﻿string[] words = new string[5] {"Hello", "my", "name", "is", "Sanya"};
+﻿string[] words = new string[6] {"Hello", "my", "name", "is", "Sanya", "!"};
 string[] needlyWords = new string[words.Length];
 
-void GetNeedlyWords(string[] array1, string[] array2)
+GetNeedlyWords(words, needlyWords);
+Console.WriteLine("Массив из строк с длинной элементов 3 = ");
+PrintNeedlyWords(needlyWords);
+
+void GetNeedlyWords(string[] array, string[] array1)
 {
     int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-    if(array1[i].Length <= 3)
+    if(array[i].Length <= 3)
         {
-        array2[count] = array1[i];
+        array1[count] = array[i];
         count++;
         }
     }
+}
+
+void PrintNeedlyWords(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
